1be768e98bece3560e9605cb17aa6487ab6556f2
tramanh243 <128776706+tramanh243@users.noreply.github.com>
2023-03-30 15:37:22 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+
+if (burgerMenu && navbarMenu) {
+    burgerMenu.addEventListener("click", () => {
+        burgerMenu.classList.toggle("is-active");
+        navbarMenu.classList.toggle("is-active");
+    });
+}
+
+document.querySelectorAll(".menu-link").forEach((link) => {
+    link.addEventListener("click", () => {
+        burgerMenu.classList.remove("is-active");
+        navbarMenu.classList.remove("is-active");
+    });
+});
+
+window.addEventListener("scroll", () => {
+    if (this.scrollY >= 85) {
+        headerMenu.classList.add("on-scroll");
+    } else {
+        headerMenu.classList.remove("on-scroll");
+    }
+});
